6edbe1e1628b4092cf5ddbd0040d72450c77b4f9
Samir Aniz <31242039+SamZuckerberg@users.noreply.github.com>
2018-10-06 00:04:56 -0300

    isNumber criado

--- src/validator/Validator.java
+++ src/validator/Validator.java
@@ -13,57 +13,82 @@ import java.util.Arrays;
  * @author samzu
  */
 public class Validator {
+
     private String form;
     String iden[];
     private char validOperations[] = {'+', '-', '/', '*'};
-    
+
     /**
      * Index
      */
-    public void index(){
+    public void index() {
         Scanner buffer = new Scanner(System.in);
         Scanner buffer2 = new Scanner(System.in);
-        
+
         String in = "Easter egg";
-        int n;
-        
+        int n=0;
+
         System.out.println("Digite a quantidade de identificadores: (NUMERO CARALHO)");
-        n = buffer.nextInt();
-        
-        this.iden = new String[n];
-        
-        for (int i = 0; i < n; i++) {
-          System.out.println("Digite a atribuição " + (i+1));
-          in = buffer2.nextLine();
-          String sub[] = in.split("=");
-          
-          if(!this.isLowerCase(sub[0])){
-              System.out.println("Variaveis somente em minusculo e sem espaço!");
-              System.exit(0);
-          }
-          this.iden[i] = in;
+        try{
+            n = buffer.nextInt();
+        }
+        catch(Exception e){
+            System.out.println("Somente números!");
+            System.exit(0);
         }
         
+
+        this.iden = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Digite a atribuição " + (i + 1));
+            in = buffer2.nextLine();
+            String sub[] = in.split("=");
+
+            if (!this.isLowerCase(sub[0]) || !this.isNumber(sub[1])) {
+                System.out.println("Arquivo inválido !");
+                System.exit(0);
+            }
+            
+            this.iden[i] = in;
+        }
+
         for (String teste : this.iden) {
             System.out.println(teste);
         }
     }
-    
+
     /**
      * Check if the String is Lower Case
-     * 
-     * @param value 
+     *
+     * @param value
      */
-    private boolean isLowerCase(String value){
+    private boolean isLowerCase(String value) {
         value = value.trim();
         char array[] = value.toCharArray();
-        
+
         for (char sub : array) {
-            System.out.println(Character.isLowerCase(sub));
-            
-            if(!Character.isLowerCase(sub))
+
+            if (!Character.isLowerCase(sub)) {
                 return false;
+            }
         }
+        return true;
+    }
+
+    /**
+     * Check if the String is number
+     *
+     * @param value
+     * @return
+     */
+    private boolean isNumber(String value) {
+        try {
+            Double num = Double.parseDouble(value);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
         return true;
     }
 }
